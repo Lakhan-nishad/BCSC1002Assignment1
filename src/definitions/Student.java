5e159fdc1f27d937929855e59bf802f2c27d1f3c
Lakhan <lakhan.nishad_cs19@gla.ac.in>
2020-08-25 11:35:03 +0530

    add all getter and setter methods of the fields for Student.java.

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -11,4 +11,36 @@ public class Student {
     private long universityRollNumber;
     private int numberOfBookIssued;
     private Book[] nameOfBooks;
+
+    public Book[] getNameOfBooks() {
+        return nameOfBooks;
+    }
+
+    public void setNameOfBooks(Book[] nameOfBooks) {
+        this.nameOfBooks = nameOfBooks;
+    }
+
+    public long getUniversityRollNumber() {
+        return universityRollNumber;
+    }
+
+    public void setUniversityRollNumber(long universityRollNumber) {
+        this.universityRollNumber = universityRollNumber;
+    }
+
+    public int getNumberOfBookIssued() {
+        return numberOfBookIssued;
+    }
+
+    public void setNumberOfBookIssued(int numberOfBookIssued) {
+        this.numberOfBookIssued = numberOfBookIssued;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
 }
