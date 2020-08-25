fde7eec031f2c2a99cc2db439f5df7abb04245aa
Lakhan <lakhan.nishad_cs19@gla.ac.in>
2020-08-25 11:45:54 +0530

    add equals(), hashCode, toString() methods in Student.java.

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -1,10 +1,13 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
+ *  User: Lakhan
  *  Date: 21/08/20
  *  Time: 3:49 PM
  *  File Name : Student.java
  * */
 package definitions;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Student {
     private String studentName;
@@ -42,5 +45,33 @@ public class Student {
 
     public void setStudentName(String studentName) {
         this.studentName = studentName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentName='" + studentName + '\'' +
+                ", universityRollNumber=" + universityRollNumber +
+                ", numberOfBookIssued=" + numberOfBookIssued +
+                ", nameOfBooks=" + Arrays.toString(nameOfBooks) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return getUniversityRollNumber() == student.getUniversityRollNumber() &&
+                getNumberOfBookIssued() == student.getNumberOfBookIssued() &&
+                Objects.equals(getStudentName(), student.getStudentName()) &&
+                Arrays.equals(getNameOfBooks(), student.getNameOfBooks());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(getStudentName(), getUniversityRollNumber(), getNumberOfBookIssued());
+        result = 31 * result + Arrays.hashCode(getNameOfBooks());
+        return result;
     }
 }
